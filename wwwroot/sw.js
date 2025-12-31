ac35e159b407f772a99c7e44d5e0a98f9e6d62a0
musabalajb-png <musabalajb@gmail.com>
2025-12-31 14:14:18 +0200

    Update sw.js

--- wwwroot/sw.js
+++ wwwroot/sw.js
@@ -1,2 +1,11 @@
-self.addEventListener('install', (e) => { console.log('Shifa App Installed'); });
-self.addEventListener('fetch', (e) => { e.respondWith(fetch(e.request)); });
+// ملف sw.js
+const cacheName = 'shifa-v1';
+const assets = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
