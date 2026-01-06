6e21ca23e51d08a17d47fd251e7964fe114af352
musabalajb-png <musabalajb@gmail.com>
2026-01-06 22:06:57 +0200

    Update Labtest.cs

--- Models/Labtest.cs
+++ Models/Labtest.cs
@@ -5,9 +5,15 @@ namespace Alshifa_clinic_galgany.Models
         public int Id { get; set; }
         public int PatientId { get; set; }
         public string TestName { get; set; } = string.Empty;
+        public string TestType { get; set; } = string.Empty;
         public string Result { get; set; } = string.Empty;
+        public string NormalRange { get; set; } = string.Empty;
         public string Status { get; set; } = "Pending";
         public DateTime RequestDate { get; set; } = DateTime.Now;
         public DateTime? ResultDate { get; set; }
+        public decimal Price { get; set; } = 0;
+        public string TechnicianName { get; set; } = string.Empty;
+        
+        public Patient Patient { get; set; }
     }
 }
