f641ec63cb08729061ba7f125cc21b839ffe1cb3
musabalajb-png <musabalajb@gmail.com>
2026-01-06 18:53:32 +0200

    Update PharmacyTransaction.cs

--- Models/PharmacyTransaction.cs
+++ Models/PharmacyTransaction.cs
@@ -1,14 +1,13 @@
-using System;
-
 namespace Alshifa_clinic_galgany.Models
 {
     public class PharmacyTransaction
     {
         public int Id { get; set; }
-        public int MedicationId { get; set; } // رقم الدواء المباع
-        public int QuantitySold { get; set; } // الكمية الصرفها الصيدلي
-        public decimal TotalAmount { get; set; } // (الكمية * سعر البيع)
-        public decimal Profit { get; set; } // (سعر البيع - سعر الشراء) * الكمية
-        public DateTime TransactionDate { get; set; } = DateTime.Now; // تاريخ العملية
+        public int PatientId { get; set; }
+        public int MedicationId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal Profit { get; set; }
+        public DateTime TransactionDate { get; set; } = DateTime.Now;
     }
 }
