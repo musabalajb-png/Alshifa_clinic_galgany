60e63a0af364648863fb7ac7f56bc07deb2b0922
musabalajb-png <musabalajb@gmail.com>
2026-01-06 22:09:35 +0200

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -7,12 +7,13 @@ namespace Alshifa_clinic_galgany.Models
         public string Password { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        public int? DepartmentId { get; set; }
         public string Email { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public bool IsActive { get; set; } = true;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime? LastLogin { get; set; }
         
-        public ICollection<SystemLog> SystemLogs { get; set; } = new List<SystemLog>();
+        public Department Department { get; set; }
     }
 }
