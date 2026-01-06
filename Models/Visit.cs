96bbf4d8344b0f14732a79e87ed1466993aa5e75
musabalajb-png <musabalajb@gmail.com>
2026-01-06 22:05:46 +0200

    Update Visit.cs

--- Models/Visit.cs
+++ Models/Visit.cs
@@ -7,8 +7,11 @@ namespace Alshifa_clinic_galgany.Models
         public DateTime VisitDate { get; set; } = DateTime.Now;
         public string Complaint { get; set; } = string.Empty;
         public string Diagnosis { get; set; } = string.Empty;
-        public string Prescription { get; set; } = string.Empty;
-        public decimal Fee { get; set; }
+        public string Treatment { get; set; } = string.Empty;
+        public decimal Fee { get; set; } = 2000;
         public string DoctorNotes { get; set; } = string.Empty;
+        public string DoctorName { get; set; } = string.Empty;
+        
+        public Patient Patient { get; set; }
     }
 }
